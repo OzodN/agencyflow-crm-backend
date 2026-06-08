060ee8170fcd1953cbeb88df383fba127dd0e457
OzodN <ytnuritdinov@gmail.com>
2026-06-08 17:30:15 +0500

    initial

--- src/main/java/com/agencyflow/crm/AgencyflowCrmApplication.java
+++ src/main/java/com/agencyflow/crm/AgencyflowCrmApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AgencyflowCrmApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AgencyflowCrmApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AgencyflowCrmApplication.class, args);
+    }
 
 }
