bb885c85a42fbad8849443f04cbac9c691dd74ca
OzodN <ytnuritdinov@gmail.com>
2026-06-12 22:03:21 +0500

    add: create sales manager user

---
+++ src/main/resources/db/migration/V12__insert_sales_manager_user.sql
@@ -0,0 +1,15 @@
+INSERT INTO users (first_name,
+                   last_name,
+                   email,
+                   password,
+                   role,
+                   active
+)
+VALUES (
+        'Sales',
+        'Manager',
+        'sales-manager@agencyflow.com',
+        '$2a$12$YiPuNatZJeu2y8t4P20ZWOzNGO5qgonx7MwW3W3Yw8Mk8I8SUgaKu',
+        'SALES_MANAGER',
+        true
+       );
